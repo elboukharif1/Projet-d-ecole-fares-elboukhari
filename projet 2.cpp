0ff17a3ff72c35da4a4320f42ab5abf21ed6e1f8
Fares elboukhari <95243744+elboukharif1@users.noreply.github.com>
2023-02-26 11:44:58 +0100

    Create projet 2.cpp

---
+++ projet 2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+///{   
+///int score(0);
+///char question 
+   ///switch (question)
+   ////{
+	   ///case '1':cout<<"voici le premier menu de question:"<<endl;
+		          ///cout<<"question 1: Quel pays a pour capitale Tirana ?"<<endl;
+		          ///cout<<"reponse 1:Nicaragua/n reponse 2: 
+   ///break;
+   ///case '*':cout<<debut*2<<endl;
+ ///break;
+   ///case '-':cout<<debut-4<<endl;
+   ///break;
+   ///default:cout<<"je sais pas quoi foutre de ton truc "<<endl;
+   ///break;
+   ///}
+
+///return 0;
+   /// }
+
+{   
+	int tabEntiers[15]={11,34,56,98,30,25,46,12,16};
+	for(int i=0;i<16;i++)
+	{
+		cout<<tabEntiers[i]<<endl;
+	}
+return 0;
+}
