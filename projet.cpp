5a56247e8cf6aebcbf4ca101eb49159827937d17
Fares elboukhari <95243744+elboukharif1@users.noreply.github.com>
2023-02-26 11:42:23 +0100

    Create projet.cpp

---
+++ projet.cpp
@@ -0,0 +1,95 @@
++#include <iostream>
+
+using namespace std;
+
+int main()
+///{
+   //// cout << "Hello World!" << endl;
+    ////return 0;
+///}
+
+///int main()
+ ///{
+  ///int cm(0);
+  ///int perimetre(0);
+  ///int surface(0);
+  ///cout<<"donner une valeur en metre de votre champ:"; cin>>cm;
+  ///perimetre = cm*4;
+  ///surface = cm*cm;
+  ///cout<<"le perimetre est de:"<<perimetre <<endl;
+  ///cout<< "la surface est de:"<<surface <<endl;
+///}
+
+///{   
+
+///int tabEntiers[15]={11,34,56,98,30,25,46,12,16};
+///cout<< tabEntiers[0]<<endl;
+///cout<< tabEntiers[1]<<endl;
+///cout<< tabEntiers[2]<<endl;
+///cout<< tabEntiers[3]<<endl;
+///cout<< tabEntiers[4]<<endl;
+///cout<< tabEntiers[5]<<endl;
+///cout<< tabEntiers[6]<<endl;
+///cout<< tabEntiers[8]<<endl;
+///cout<< tabEntiers[7]<<endl;
+
+
+///return 0;
+   /// }
+
+///{   
+///int a (0);
+///cout<< "quand a ete elu francois mitterand ?" <<endl;
+///cout<< "1.le 21 mai 81"<<endl;
+///cout<< "2.le 5 mars 81"<<endl;
+///cout<< "3.le 5 avril 81!"<<endl;
+///cout<< "4.le 20 octobre 81 !"<<endl;
+///cout<< "Repond au question:";cin>>a;
+///if (a==1)
+///{
+  ///cout<<"c'est ca "<<endl;
+  ///}
+///else
+///{
+      ///cout<<"faux!!!!!!!!!!"<<endl;
+    ///}
+///return 0;
+  ///}
+  
+  
+  {
+   int debut(0);
+   char op;
+   cout<<"donner une valeur :";cin>>debut;
+   cout<<"donner un signe de calcul tels que +,-,*: ";cin>>op;
+   switch (op)
+   {
+   case '+':cout<<debut+1<<endl;
+   break;
+   case '*':cout<<debut*2<<endl;
+   break;
+   case '-':cout<<debut-4<<endl;
+   break;
+   default:cout<<"je sais pas quoi foutre de ton truc "<<endl;
+   break;
+   }
+   return 0;
+   }
+  
+
+///{
+ ///int compteur(0);
+ ///while (compteur<10)
+ ///{
+  ///compteur++;
+  ///cout<<compteur<<endl;
+ ///}
+ ///}
+ ///{   
+	///int tabEntiers[14]={11,34,56,98,30,25,46,12,16,23,12,78,56,87};
+	///for(int i=0;i<15;i++)
+	///{
+	////cout<<tabEntiers[i]<<endl;
+	 ///}
+///return 0;
+ ///}
